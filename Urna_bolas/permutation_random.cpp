faf1c3c27b5ef90b37f170c9cf7bd2e243fa7b96
Pedro Henrique Rodrigues de Lima <85079468+PedroHenrique-dev@users.noreply.github.com>
2022-10-05 21:04:37 -0300

    Update

--- Urna_bolas/permutation_random.cpp
+++ Urna_bolas/permutation_random.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <random>
 #include <utility>
+#include <ctime>
 
 #include "permutation_random.h"
 
@@ -15,11 +16,33 @@ double numRandom()
     return dist(mt);
 }
 
+double lcg_n1_0_1(int semente)
+{
+    int a = 163, c = 151, m = 251;
+
+    double aux = (a * semente + c) % m;
+    while (aux <= 0 || aux >= 1)
+    {
+        aux /= 10;
+    }
+
+    return aux;
+}
+
 template <class LIST>
-LIST permutation(LIST x)
+LIST permutation(LIST x, bool bool_lcg)
 {
     int n = x.size();
-    double u = numRandom();
+
+    double u;
+    if (bool_lcg)
+    {
+        u = lcg_n1_0_1(time(NULL));
+    }
+    else
+    {
+        u = numRandom();
+    }
 
     int k{n-1};
     while (k > -1)    
